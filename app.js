ac356c396b047b7f455714b77c1b8e85ba4a0ac4
adityaa72 <adityah1357908642@gmail.com>
2024-08-23 17:18:48 +0530

    feat: to pnpm

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ require("dotenv").config({
 });
 
 const app = express();
-const PORT = process.env.PORT || 8080;
-console.log("PORT->", PORT)
+const PORT = process.env.PORT || 8001;
+console.log("PORT->", PORT);
 
 app.get("/", (req, res, next) => {
+  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  console.log("headers", req.headers);
+  console.log("ip", ip);
   return res.send("i am listening here");
 });
 
 app.listen(PORT, () => {
-  console.log("Listening on port " + PORT);
+  console.log("Listening on port http://localhost:" + PORT);
 });
